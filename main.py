2d5694edb02ae5c2ef9130b95601f799e6380699
student06 <jakub.gola2@teb.edu.pl>
2024-12-05 11:22:40 +0100

    05.12.2024

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
+from array import array
 from random import random
 
+class Table:
+    def __init__(self):
+        tab = [[0 for i in range(5) for i in range(5)]]
 
-def print(name):
+    def show(tab):
+        for i in range()
+            print(i)
 
+        for n in range(5):
+            for m in range(5):
+                tab[n][m] = random.randint(0,10)
+
+    def sum(self):
+        return sum(tab.tab)
 
 
 if __name__ == '__main__':
+    tab = Table()
+    tab.tab = [random() for i in range(10)]
+    tab.show()
+    print(tab.sum())
